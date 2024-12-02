44b828c57a958cdf4279258867fb4f2a43671cdb
Adam Sims <adamsims1203@gmail.com>
2024-12-02 18:44:47 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bizurk-game',
+  webDir: 'www'
+};
+
+export default config;
